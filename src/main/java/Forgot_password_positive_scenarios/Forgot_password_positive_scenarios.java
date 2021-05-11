937bc4be93b5bf5423577e61fcc476d433864917
Sariga Sajeev <53913474+SarigaMallika@users.noreply.github.com>
2021-05-11 12:02:15 +0530

    Update Forgot_password_positive_scenarios.java

--- src/main/java/Forgot_password_positive_scenarios/Forgot_password_positive_scenarios.java
+++ src/main/java/Forgot_password_positive_scenarios/Forgot_password_positive_scenarios.java
@@ -24,11 +24,10 @@ public class Forgot_password_positive_scenarios extends Generic_function{
 
 	/* TC_001 -Validate that the  user should  able to enter 10 digit number in "Phone Number" field*/
 	@Then("Click on Login button then click on Forgot Password link and enter phone number in Phone number field")
-	public static void forgot_positive_tc_001(){
+	public static void forgot_positive_tc_001() throws Exception{
 		try {
 			driver.findElement(By.xpath(OR_reader("Object_Locator","welcome_login"))).click();	
-			driver.findElement(By.xpath(OR_reader("Object_Locator","forgot_password_link"))).click();	
-			driver.findElement(By.xpath(OR_reader("Object_Locator","forgot_password_phone_no"))).click();	
+			driver.findElement(By.xpath(OR_reader("Object_Locator","forgot_password_link"))).click();		
 			driver.findElement(By.xpath(OR_reader("Object_Locator","forgot_password_phone_no"))).sendKeys(td_reader("forgot_password_phone_no",3));
 			str= driver.findElement(By.xpath(OR_reader("Object_Locator","forgot_password_page_title"))).getText();
 			Assert.assertEquals(str,td_reader("forgot_password_page_title"));
@@ -36,12 +35,13 @@ public class Forgot_password_positive_scenarios extends Generic_function{
 			browser_refresh();
 		} catch (Exception e) {
 			e.printStackTrace();
+			takeScreenShot("forgot_positive_tc_001");
 		}
 	}
 
 	/* TC_002 -Validate that the user should able to click on the  Send Reset Link button on entering valid phone number in Phone number field*/
 	@Given("Verify that Send Reset Link button gets enabled and user can click on it")
-	public static void forgot_positive_tc_002(){
+	public static void forgot_positive_tc_002() throws Exception{
 		try {
 			driver.findElement(By.xpath(OR_reader("Object_Locator","forgot_password_phone_no"))).sendKeys(td_reader("forgot_password_phone_no",3));
 			driver.findElement(By.xpath(OR_reader("Object_Locator","send_reset_link"))).click();
@@ -49,12 +49,13 @@ public class Forgot_password_positive_scenarios extends Generic_function{
 			Assert.assertEquals(str,td_reader("forgot_password_page_title"));
 		} catch (Exception e) {
 			e.printStackTrace();
+			takeScreenShot("forgot_positive_tc_002");
 		}
 	}
 
 	/* TC_003 -Validate that the user is navigated to sign up page on clicking Sign up link*/
 	@Given("Click on Sign up link then should be navigated  to  Sign up page")
-	public static void forgot_positive_tc_003(){
+	public static void forgot_positive_tc_003() throws Exception{
 		try {
 			browser_back();
 			driver.findElement(By.xpath(OR_reader("Object_Locator","forgot_password_signup_link"))).click();			
@@ -63,6 +64,7 @@ public class Forgot_password_positive_scenarios extends Generic_function{
 			browser_close();
 		} catch (Exception e) {
 			e.printStackTrace();
+			takeScreenShot("forgot_positive_tc_003");
 		}
 		
 	}
